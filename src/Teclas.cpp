07ca5fba79002ae14ddc249c65adc0988e8adf9a
geraldvindas <geraldvindas2010@gmail.com>
2017-07-12 22:34:05 -0600

    nueva clase teclas .cpp

--- src/Teclas.cpp
+++ src/Teclas.cpp
@@ -1,6 +1,76 @@
 #include "teclas.h"
 
+
 Teclas::Teclas()
 {
+
+}
+
+void Teclas::agregarTeclado()
+{
+    v_teclado.append("letraQ");
+    v_teclado.append("letraw");
+    v_teclado.append("letraE");
+    v_teclado.append("letraR");
+    v_teclado.append("letraT");
+    v_teclado.append("letraY");
+    v_teclado.append("letraU");
+    v_teclado.append("letraI");
+    v_teclado.append("letraO");
+    v_teclado.append("letraP");
+    v_teclado.append("letraA");
+    v_teclado.append("letraS");
+    v_teclado.append("letraD");
+    v_teclado.append("letraF");
+    v_teclado.append("letraG");
+    v_teclado.append("letraH");
+    v_teclado.append("letraJ");
+    v_teclado.append("letraK");
+    v_teclado.append("letraL");
+    v_teclado.append("letraenye");
+    v_teclado.append("letraZ");
+    v_teclado.append("letraX");
+    v_teclado.append("letraC");
+    v_teclado.append("letraV");
+    v_teclado.append("letraB");
+    v_teclado.append("letraN");
+    v_teclado.append("letraM");
+}
+
+#include <iostream>
+
+void Teclas::mostrarTeclado(QSvgRenderer *svgRenderer, QGraphicsScene *escena)
+{
+    /// objeto temporal para cargar las teclas
+    ObjetoProp* temporal;
+
+
+    /// manejar posiciones
+    int ejeX=90,ejeY=50;
+
+    /// ciclo que recorre el vector con los ID de cada objeto
+    /// Y crea un ObjetoProp a partir de ellos.
+    for(int indice=0;indice < v_teclado.size();++indice){
+        /// controlar las posiciones de las teclas
+        if(ejeX<=190){
+            ejeX+=10;
+        }else{
+            ejeX=90;
+            ejeY+=10;
+        }
+        if(ejeY==70&&ejeY==90)
+            ejeY=100;
+
+        /// asignando propiedades a las teclas
+        ///
+        temporal= new ObjetoProp(v_teclado[indice]);
+        temporal->setElementId(v_teclado[indice]);
+        temporal->setSharedRenderer(svgRenderer);
+        temporal->setZValue(1);
+        temporal->setPos(ejeX,ejeY);
+        escena->addItem(temporal);
+        v_objetosProp.append(temporal);
+        std::cout<<indice<<": Element loaded("<<v_teclado[indice].toStdString()<<")"<<std::endl;
+    }
 
 }
