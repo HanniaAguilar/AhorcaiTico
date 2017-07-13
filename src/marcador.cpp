0c2fa1efa7070ae5a9187fe765c665ccbfec8607
geraldvindas <geraldvindas2010@gmail.com>
2017-07-12 22:36:21 -0600

    marcador del juego
    Nueva clase marcador

---
+++ src/marcador.cpp
@@ -0,0 +1,35 @@
+#include "marcador.h"
+#include <QColor>
+#include <QFont>
+
+Marcador::Marcador(const QString &tipoMarcador, int puntosIniciales, QColor color, QGraphicsItem* parent)
+    : QGraphicsTextItem(parent)
+    , tipoMarcador(tipoMarcador)
+    , puntos(puntosIniciales)
+{
+    setDefaultTextColor(color);
+    setFont(QFont("", 24));
+    actualizarMarcador();
+}
+
+Marcador::~Marcador()
+{
+
+}
+
+void Marcador::incrementePuntos()
+{
+    puntos+=10;
+    actualizarMarcador();
+}
+
+void Marcador::actualizarMarcador()
+{
+    setPlainText( QString("%1: %2").arg(tipoMarcador).arg(puntos) );
+}
+
+void Marcador::restablecerMarcador()
+{
+    puntos=0;
+    actualizarMarcador();
+}
---
+++ src/marcador.h
@@ -0,0 +1,33 @@
+#ifndef MARCADOR_H
+#define MARCADOR_H
+
+#include <QGraphicsTextItem>
+
+class Marcador : public QGraphicsTextItem
+{
+    Q_OBJECT
+
+   protected:
+    /// marcador de puntos del jugador
+    QString tipoMarcador;
+    /// Cantidad de puntos ganados
+    int puntos;
+
+   public:
+    /// constructor
+    Marcador(const QString& tipoMarcador, int puntosIniciales, QColor color, QGraphicsItem *parent = nullptr);
+    /// destructor
+    ~Marcador();
+    /// metodo get de Puntos
+    inline int getPuntos() { return puntos;}
+    /// incrementar puntos
+    void incrementePuntos();
+    /// resetear el marcador
+    void restablecerMarcador();
+
+   protected:
+    void actualizarMarcador();
+
+};
+
+#endif // MARCADOR_H
