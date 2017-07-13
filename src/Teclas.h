18bf69bb81915ed14f3e5411f3b431b47f34c513
geraldvindas <geraldvindas2010@gmail.com>
2017-07-12 22:34:51 -0600

    nuevo .h teclas

--- src/Teclas.h
+++ src/Teclas.h
@@ -1,11 +1,28 @@
 #ifndef TECLAS_H
 #define TECLAS_H
 
+#include "objetoprop.h"
+#include <QGraphicsScene>
+#include <QPropertyAnimation>
+#include <QSvgRenderer>
+#include <QVector>
+#include <QString>
+#include <algorithm>
+#include <random>
+#include <chrono>
 
 class Teclas
 {
-public:
+   protected:
+    QVector<QString> v_teclado;
+
+   public:
+    QVector<ObjetoProp*> v_objetosProp;
+
+   public:
     Teclas();
+    void agregarTeclado();
+    void mostrarTeclado(QSvgRenderer* svgRenderer, QGraphicsScene *escena);
 };
 
 #endif // TECLAS_H
