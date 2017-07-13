8e6c23d41582666f1e8974ed8ac23832540841f2
geraldvindas <geraldvindas2010@gmail.com>
2017-07-12 22:19:38 -0600

    Actualizacion del .h mainWindow

--- src/MainWindow.h
+++ src/MainWindow.h
@@ -6,6 +6,7 @@
 #include <QGraphicsScene>
 #include <QSvgRenderer>
 #include "objetoprop.h"
+#include "teclas.h"
 
 
 class Controlador;
@@ -16,11 +17,14 @@ class MainWindow:public QGraphicsView
     protected:
       ObjetoProp* m_componente;
       QSvgRenderer* m_svgRenderer;
+      ObjetoProp* m_fondo;
+      Teclas* teclas;
 
     public:
         MainWindow(QGraphicsScene* escena);
         ~MainWindow();
         void insertarComponentes(QGraphicsScene* escena);
+        void dibujeFondo(QGraphicsScene* m_escena);
 };
 
 #endif // MAINWINDOW_H
